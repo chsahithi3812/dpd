17b4aa16c5254119624e9ce06e6ffdabe5ad6655
Ch Sahithi <chsahithi.3812@gmail.com>
2023-03-03 14:37:30 +0530

    task

--- server/models/todo.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  name: [String],
-});
-const model = new mongoose.model("Model", userSchema);
-module.exports = model;
--- server/routes/todoRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { add, get, deleteItem } = require("../controllers/todoController");
-
-router.post("/add", add);
-router.get("/", get);
-router.delete("/delete/:key", deleteItem);
-
-module.exports = router;
